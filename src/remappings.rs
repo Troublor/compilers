66d2677e9257ec6b9aee99ad4ab894375ee6af22
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-04-13 02:24:04 +0200

    Update remappings.rs
    Co-authored-by: Blaze <blaze@dorime.org>

--- src/remappings.rs
+++ src/remappings.rs
@@ -17,7 +17,7 @@ const JS_LIB_DIR: &str = "node_modules";
 fn to_slash_path_preserve_trailing_slash(path: &str) -> String {
     use path_slash::PathExt;
     let has_trailing_slash = path.ends_with('/') || path.ends_with('\\');
-    let mut p = Path::new(path).to_slash_lossy().into_string();
+    let mut p = Path::new(path).to_slash_lossy().into_owned();
     if has_trailing_slash {
         p.push('/');
     }
